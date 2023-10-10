222e64210e1e1d30a4966e6d9a80c10aa7c27a7a
Henrique Oliveira Silva <hiky_silva@hotmail.com>
2023-10-10 19:14:37 -0300

    desafio de projeto ContaTerminal finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,9 +7,28 @@ public class ContaTerminal {
         // Obter pela classe Scanner os valores do terminal
         // Exibir a mensagem da conta criada
 
-        Scanner leitor = new Scanner(System.in);
-        System.out.println();
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
 
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta");
+        numero = leitor.nextInt();
+
+        System.out.println("Por favor, digite o número da agencia");
+        agencia = leitor.next();
+
+        System.out.println("Por favor, digite o seu nome");
+        nomeCliente = leitor.nextLine();
+        nomeCliente += leitor.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo");
+        saldo = leitor.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo "
+        + saldo + " já está disponível para saque.");
 
     }
 }
