1524905702f298a06fb2dfe5d6c13b094201c32f
Henrique Oliveira Silva <hiky_silva@hotmail.com>
2023-10-10 18:23:57 -0300

    primeiro commit ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main (String [] ars){
+        //TODO: Conhecer e importar a classe Scanner
+        // Exibir as mensagens para nosso usuário
+        // Obter pela classe Scanner os valores do terminal
+        // Exibir a mensagem da conta criada
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println();
+
+
+    }
+}
